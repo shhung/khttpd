1712254cf7110ae96a51607c3c4141d2b3bb94a0
shhung <shhung@csie.io>
2023-05-06 23:18:45 +0800

    Add CMWQ into http server

--- http_server.h
+++ http_server.h
@@ -1,10 +1,23 @@
 #ifndef KHTTPD_HTTP_SERVER_H
 #define KHTTPD_HTTP_SERVER_H
 
+#include <linux/module.h>
+#include <linux/workqueue.h>
 #include <net/sock.h>
 
 struct http_server_param {
     struct socket *listen_socket;
+};
+
+struct http_service {
+    bool is_stopped;
+    struct list_head worker;
+};
+
+struct khttpd {
+    struct socket *sock;
+    struct list_head list;
+    struct work_struct khttpd_work;
 };
 
 extern int http_server_daemon(void *arg);
